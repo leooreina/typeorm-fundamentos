adb520f48ea137f60caf3bafbdad11dae987416d
Leonardo Reina <leonardo@formahomolog.com.br>
2020-08-13 22:36:37 -0300

    WIP first table transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,32 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category_id: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    return await transactionRepository.save(transaction);
   }
 }
 
